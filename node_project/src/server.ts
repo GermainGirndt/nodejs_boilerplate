4cf7df84b7a262c6acfe4520f3ce945ac11eefa2
GermainPereira <pereira.germain@outlook.com>
2020-06-22 20:06:59 -0300

    Refactored code

--- node_project/src/server.ts
+++ node_project/src/server.ts
@@ -1,12 +1,11 @@
-import express from "express";
+import express from 'express';
 // src/server.ts
-import routes from "./routes/index";
+import routes from './routes';
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello GoStack" });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
-    console.log("Server started on port 3333!");
+    console.log('Server started on port 3333!');
 });
